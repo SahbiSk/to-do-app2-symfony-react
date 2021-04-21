e58f71d63e05e2af5313864e59f905eefb82c556
Sahbi <sahbici1000@gmail.com>
2021-04-21 12:52:56 +0100

    added todo table list

--- assets/components/TodoTable.js
+++ assets/components/TodoTable.js
@@ -1,14 +1,54 @@
+import {
+  IconButton,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TextField,
+} from "@material-ui/core";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
+import AddIcon from "@material-ui/icons/Add";
 import React, { useContext } from "react";
 import { TodoContext } from "../contexts/TodoContext";
 
 const TodoTable = () => {
   const context = useContext(TodoContext);
   return (
-    <div>
-      {context.todos.map((e, i) => (
-        <h1 key={i}>{e.task} </h1>
-      ))}
-    </div>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Task</TableCell>
+          <TableCell align="right">Actions</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        <TableRow>
+          <TableCell>
+            <TextField fullWidth={true} />
+          </TableCell>
+          <TableCell align="right">
+            <IconButton>
+              <AddIcon />
+            </IconButton>
+          </TableCell>
+        </TableRow>
+        {context.todos.map((todo, key) => (
+          <TableRow key={key}>
+            <TableCell>{todo.name} </TableCell>
+            <TableCell align="right">
+              <IconButton>
+                <EditIcon />
+              </IconButton>
+              <IconButton>
+                <DeleteIcon />
+              </IconButton>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   );
 };
 
--- assets/contexts/TodoContext.js
+++ assets/contexts/TodoContext.js
@@ -3,7 +3,25 @@ import React, { createContext, useState } from "react";
 export const TodoContext = createContext();
 
 const TodoContextProvider = (props) => {
-  const [todos, setTodos] = useState([{ task: "do something" }]);
+  const [todos, setTodos] = useState([
+    { name: "do something" },
+    { name: "do something" },
+    { name: "do something" },
+    { name: "do something" },
+    { name: "do something" },
+    { name: "do something" },
+    { name: "do something" },
+    { name: "do something" },
+    { name: "do something" },
+    { name: "do something" },
+    { name: "do something" },
+    { name: "do something" },
+    { name: "do something" },
+    { name: "do something" },
+    { name: "do something" },
+    { name: "do something" },
+    { name: "do something" },
+  ]);
 
   //create
   function createTodo() {}
